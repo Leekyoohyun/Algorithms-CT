acd3dc54c6f7568596f383e2cc9d0853ca7121d2
kyoohyunlee <khyhlee@naver.com>
2025-01-29 17:24:01 +0900

    Sorting only digits

---
+++ src/strings/SortStrings.java
@@ -0,0 +1,22 @@
+package src.strings;
+import java.util.Arrays;
+
+public class SortStrings {
+    public static int[] solution(String my_string) {
+
+        int i= 0;
+        String newString = my_string.replaceAll("[a-z]","");
+        String[] arr = newString.split("");
+        int[] answer = new int[newString.length()];
+        for(String s : arr){
+            answer[i] = Integer.parseInt(s);
+            i++;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("sd8das9dasfaas9989saf8")));
+    }
+}
