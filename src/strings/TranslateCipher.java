8cd96bd7702ade5d9465ff1054747fdce9d7e6e2
kyoohyunlee <khyhlee@naver.com>
2025-01-30 14:05:47 +0900

    split 메소드

---
+++ src/strings/TranslateCipher.java
@@ -0,0 +1,27 @@
+package src.strings;
+
+/*
+* 암호화된 문자열 cipher를 주고받습니다.
+* 그 문자열에서 code의 배수 번째 글자만 진짜 암호입니다.
+*/
+
+public class TranslateCipher {
+    public static String solution(String cipher, int code) {
+        /**
+         * @param cipher - 암호화된 문자열
+         * @param code - code의 배수 번째 문자만 진짜 문자
+         **/
+        String answer = "";
+        String[] list = cipher.split("");
+        for(int i=code-1; i< list.length; i += code){
+            answer += list[i];
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("dfjardstddetckdaccccdegk", 4));
+        System.out.println(solution("pfqallllabwaoclk",2));
+    }
+}
