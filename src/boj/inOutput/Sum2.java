69870819b8ff254a7c52486b871f503f00f83943
DESKTOP-RE3NEAR\user <le3867@daum.net>
2025-02-04 17:29:47 +0900

    BufferedReader, StringTokenizer

--- src/boj/inOutput/Sum2.java
+++ src/boj/inOutput/Sum2.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.nio.Buffer;
 import java.util.StringTokenizer;
 
+/*합, 차, 곱 모두 같을거고 BufferedReaderm StringTokenizer 활용예제*/
+
 public class Sum2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,5 +18,6 @@ public class Sum2 {
         int b = Integer.parseInt(st.nextToken());
 
         System.out.println(a + b);
+        System.out.println(a/b+a%b);
     }
 }
