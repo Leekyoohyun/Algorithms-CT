dd1995eec0d2662b28e97bd465cd9e3962f1f337
KYOOHYUN <khyhlee@naver.com>
2025-02-18 10:44:53 +0900

    selection sort

--- src/boj_lec/sorting/P1427.java
+++ src/boj_lec/sorting/P1427.java
@@ -5,13 +5,12 @@ package src.boj_lec.sorting;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class P1427 {
-    public static void main(String[] args) throws IOException {
+
+    // 첫번째 풀이
+    /*public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
@@ -28,5 +27,34 @@ public class P1427 {
         for(int i = arr.length-1; i >= 0; i--){
             System.out.print(arr[i] + "");
         }
+    }*/
+
+    // selection sort 쓰기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] A = new int[str.length()];
+        for(int i=0; i<str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i, i+1));
+        }
+        //선택정렬
+        for(int i=0; i<str.length(); i++) {
+            int Max = i;
+            for(int j = i+1; j<str.length(); j++) {
+                if(A[Max] < A[j]) {
+                    Max = j;
+                }
+            }
+            if(A[i] < A[Max]) {
+                int temp = A[i];
+                A[i] = A[Max];
+                A[Max] = temp;
+            }
+        }
+
+        for(int i : A){
+            System.out.print(i);
+        }
     }
+
 }
