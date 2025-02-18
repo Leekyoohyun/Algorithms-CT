dc9f65e112d02690f3499123800f011b1fac86bf
KYOOHYUN <khyhlee@naver.com>
2025-02-18 09:28:52 +0900

    슬라이딩 윈도우

--- src/boj_lec/dataStructure/P12891.java
+++ src/boj_lec/dataStructure/P12891.java
@@ -6,54 +6,87 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P12891 {
+
+    static int myArr[];
+    static int checkArr[];
+    static int checkSecret;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int result = 0;
+        checkArr = new int[4];
+        myArr = new int[4];
+        char A[] = new char[S];
+        checkSecret = 0;
 
-        st = new StringTokenizer(br.readLine(), " ");
-        int S = Math.abs(Integer.parseInt(st.nextToken())); //DNA 문자열 길이
-        int P = Math.abs(Integer.parseInt(st.nextToken())); // 비밀번호 길이
-
-        String dna = br.readLine(); //dna 문자 받기
-
-        // 최소 개수
-        st = new StringTokenizer(br.readLine(), " ");
-        int A = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int G = Integer.parseInt(st.nextToken());
-        int T = Integer.parseInt(st.nextToken());
-        int A_count = 0;
-        int C_count = 0;
-        int G_count = 0;
-        int T_count = 0;
-
-        int start = 0;
-        int end = P-1;
-        int count = 0;
-
-        for(int i = 0; i<=end && end <= S; i++) {
-            String password = dna.substring(start, end);
-            if(password.charAt(i) == 'A'){
-                A_count++;
-            }else if(password.charAt(i) == 'C'){
-                C_count++;
-            }else if(password.charAt(i) == 'G'){
-                G_count++;
-            }else if(password.charAt(i) == 'T'){
-                T_count++;
+        A = br.readLine().toCharArray();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<4; i++){
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if(checkArr[i] == 0){
+                checkSecret++;
             }
-            start++;
-            end++;
-        }
-        if(A_count >= A && C_count >= C && G_count >= G && T_count >= T ){
-            count++;
         }
 
-        System.out.println(count);
+        for(int i=0; i<P; i++){
+            Add(A[i]);
+        }
+        if(checkSecret == 4) result++;
 
+        //슬라이딩 윈도우
+        for(int i=P; i<S; i++){
+            int j=i-P;
+            Add(A[i]);
+            Remove(A[j]);
+            if(checkSecret == 4) result++;
+        }
 
+        System.out.println(result);
+        br.close();
+    }
 
+    private static void Remove(char c) {
+        switch(c){
+            case 'A':
+                if(myArr[0] == checkArr[0]) checkSecret--;
+                myArr[0]--;
+                break;
+            case 'C':
+                if(myArr[1] == checkArr[1]) checkSecret--;
+                myArr[1]--;
+                break;
+            case 'G':
+                if(myArr[2] == checkArr[2]) checkSecret--;
+                myArr[2]--;
+                break;
+            case 'T':
+                if(myArr[3] == checkArr[3]) checkSecret--;
+                myArr[3]--;
+                break;
+        }
+    }
 
-
+    private static void Add(char c) {
+        switch(c){
+            case 'A':
+                myArr[0]++;
+                if(myArr[0] == checkArr[0]) checkSecret++;
+                break;
+            case 'C':
+                myArr[1]++;
+                if(myArr[1] == checkArr[1]) checkSecret++;
+                break;
+            case 'G':
+                myArr[2]++;
+                if(myArr[2] == checkArr[2]) checkSecret++;
+                break;
+            case 'T':
+                myArr[3]++;
+                if(myArr[3] == checkArr[3]) checkSecret++;
+                break;
+        }
     }
 }
