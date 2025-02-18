a8a41a92d64ff3b586129d1c804982dfe493be24
KYOOHYUN <khyhlee@naver.com>
2025-02-18 09:52:18 +0900

    Queue 주석추가

--- src/boj_lec/dataStructure/queue/P2164.java
+++ src/boj_lec/dataStructure/queue/P2164.java
@@ -14,13 +14,13 @@ public class P2164 {
         Queue<Integer> queue = new LinkedList<>();
 
         for(int i=1; i<=N; i++) {
-            queue.offer(i);
+            queue.offer(i); // 큐에 넣어
         }
-        while (queue.size() > 1){
+        while (queue.size() > 1){ // 큐에 하나 남을때까지
             queue.poll(); //맨 앞 원소 버리기
-            queue.offer(queue.poll()); // 버려진 원소 맨 뒤에 넣기
+            queue.offer(queue.poll()); // 그 다음에 버리면서 동시에 맨 뒤에 넣기
         }
 
-        System.out.println(queue.poll()); // 마지막 원소
+        System.out.println(queue.poll()); // 마지막 원소 빼기!
     }
 }
