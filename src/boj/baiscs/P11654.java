a7dc3a9795afe1a2e5b4d23b39d5a29320c22c4e
KYOOHYUN <khyhlee@naver.com>
2025-02-27 09:57:57 +0900

    출력: 아스키코드

---
+++ src/boj/baiscs/P11654.java
@@ -0,0 +1,13 @@
+package src.boj.baiscs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int c = br.readLine().charAt(0);
+        System.out.println(c);
+    }
+}
